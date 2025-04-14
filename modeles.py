9286f6e0273dad9e1e97c179b09e9ac98a291715
Flow449 <florent.guillaud1@gmail.com>
2025-04-14 23:17:49 +0200

    Add files via upload

--- modeles.py
+++ modeles.py
@@ -1,9 +1,18 @@
 import joblib
+import pandas as pd
 
-def charger_modele():
-    return joblib.load("modele.pkl")
+def charger_modele(path='modele_avance.pkl'):
+    return joblib.load(path)
 
-def predire_resultat(modele, home_team, away_team, home_goals, away_goals):
-    features = [[home_goals, away_goals]]
+def preparer_features(home_rank, away_rank, home_form, away_form):
+    return pd.DataFrame([{
+        "home_rank": home_rank,
+        "away_rank": away_rank,
+        "home_form": home_form,
+        "away_form": away_form
+    }])
+
+def predire_resultat(modele, home_rank, away_rank, home_form, away_form):
+    features = preparer_features(home_rank, away_rank, home_form, away_form)
     prediction = modele.predict(features)
     return prediction[0]
