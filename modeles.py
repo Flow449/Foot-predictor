387f184c8e12df674792a5ede3f696d3e9337da4
Flow449 <florent.guillaud1@gmail.com>
2025-04-15 13:23:47 +0200

    Update modeles.py

--- modeles.py
+++ modeles.py
@@ -4,3 +4,11 @@ import os
 def charger_modele():
     chemin = os.path.join("models", "modele.pkl")  # <- nom du bon dossier
     return joblib.load(chemin)
+def predire_score(equipe_dom, equipe_ext, modele):
+    # exemple fictif : retourne un score fixe
+    return {
+        "equipe_dom": equipe_dom,
+        "equipe_ext": equipe_ext,
+        "score_dom": 1,
+        "score_ext": 2
+    }
