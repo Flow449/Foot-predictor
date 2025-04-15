f7a07ceb8d70e05c7b47396ad656c264521b535d
Flow449 <florent.guillaud1@gmail.com>
2025-04-15 10:39:23 +0200

    Update modeles.py

--- modeles.py
+++ modeles.py
@@ -2,5 +2,5 @@ import joblib
 import os
 
 def charger_modele():
-    chemin = os.path.join("models", "modele.pkl")
+    chemin = os.path.join("models", "modele.pkl")  # <- nom du bon dossier
     return joblib.load(chemin)
