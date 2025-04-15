949e558a6551e922fea4c55a8a1039d35dd4a93b
Flow449 <florent.guillaud1@gmail.com>
2025-04-15 09:46:55 +0200

    Add files via upload

--- modeles.py
+++ modeles.py
@@ -1,16 +1,7 @@
+
 import joblib
+import os
 
 def charger_modele():
-    chemin = "models/modele.joblib"  # chemin vers le modèle dans le dossier models
+    chemin = os.path.join("modeles", "modele.pkl")
     return joblib.load(chemin)
-
-def predire_resultat(modele, home_rank, away_rank, home_form, away_form):
-    features = [[home_rank, away_rank, home_form, away_form]]
-    prediction = modele.predict(features)[0]
-    return prediction
-
-def predire_proba(modele, home_rank, away_rank, home_form, away_form):
-    features = [[home_rank, away_rank, home_form, away_form]]
-    proba = modele.predict_proba(features)[0]
-    classes = modele.classes_
-    return dict(zip(classes, proba))
